a98ad7bef920d6814a6c811ef5bf6bc5e8a6a414
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2023-08-17 20:45:03 +0600

    auth system

--- src/modules/users/userController.js
+++ src/modules/users/userController.js
@@ -5,28 +5,32 @@ exports.createUser = async (req, res) => {
     const user = new userModel(req.body);
     console.log(user);
     const savedUser = await user.save();
-    res.json({
-      status: 'success',
-      data: savedUser,
-      message: 'user added successfully',
-    });
+    res.json({ code: 200, message: 'Signup success', user: savedUser });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.json({ code: 500, message: 'Signup Err', error: error.message });
   }
 };
 exports.createUserLogin = async (req, res) => {
   const { email, password } = req.body;
-  userModel.findOne({ email: email }).then(user => {
-    if (user) {
-      if (user.password === password) {
-        res.json('success');
+  userModel
+    .findOne({ email: email })
+    .then(result => {
+      console.log(result, '11');
+
+      if (result.password !== password) {
+        res.send({ code: 404, message: 'password wrong' });
       } else {
-        res.json('the password is incorrect');
+        res.send({
+          email: result.email,
+          code: 200,
+          message: 'user Found',
+          token: 'hfgdhg',
+        });
       }
-    } else {
-      res.json('no record found');
-    }
-  });
+    })
+    .catch(err => {
+      res.send({ code: 500, message: 'user not found', error: err });
+    });
 };
 exports.findAllUser = async (req, res) => {
   try {
